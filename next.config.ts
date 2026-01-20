df0d440793afac0c0a7bf49227f167f0e6a3a0bd
kine-drafts <aaronwiseup2@gmail.com>
2026-01-19 19:36:14 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Critical for Excalidraw to work in App Router
+  transpilePackages: ['@excalidraw/excalidraw', '@excalidraw/mermaid-to-excalidraw'],
 };
 
 export default nextConfig;
